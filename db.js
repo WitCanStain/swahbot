ba5194f6fbff94ee9e6cd2bcf2af00b8382726c6
Ruben Drayton <ruben.drayton@gmail.com>
2022-02-16 11:48:55 +0200

    modified pool creation

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 const connection_string = process.env.DATABASE_URL || `postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PORT}/${process.env.PGDATABASE}`
-const pool = new Pool(connection_string);
+const pool = new Pool({connection_string});
 
 module.exports = { pool };
 
