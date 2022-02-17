d3dbca5ed858d2e292571c2b591fc66f12204b26
Ruben Drayton <ruben.drayton@gmail.com>
2022-02-17 11:50:53 +0200

    added connect statement to db

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
 const {Client} = require("pg");
-const connection_string = process.env.DATABASE_URL// || `postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PORT}/${process.env.PGDATABASE}`
+const connection_string = process.env.DATABASE_URL || `postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PORT}/${process.env.PGDATABASE}`
 console.log(connection_string);
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
 });
+
+client.connect();
 
 module.exports = {client};
